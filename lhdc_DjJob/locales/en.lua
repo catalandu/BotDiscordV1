c0eaf6ebec0eccbfb0c00c481f24b1663b2fdbc9
catalandu <84291747+catalandu@users.noreply.github.com>
2021-11-08 04:34:57 +0100

    Add files via upload

---
+++ lhdc_DjJob/locales/en.lua
@@ -0,0 +1,16 @@
+--Name EN but language in Portuguese(PT)--
+Locales['en'] = {
+  ['cloakroom'] = 'DJ Menu',
+  ['cloak_change'] = 'Pressiona ~INPUT_PICKUP~ para o menu.',
+  ['stop_work'] = 'Parar de trabalhar',
+  ['start_work'] = 'Começar a fazer Mix`s',
+  ['foot_work'] = 'Tens de estar a pé para trabalhares.',
+  ['max_limit'] = 'Não podes carregar ~y~%s~s~',
+  ['not_enough'] = 'Não tens ~y~%s~s~ para trocar.',
+  ['delivery'] = 'Entrega/Troca',
+  ['dj_club_locker'] = 'DJ Job Menu',
+  ['dj_play_music'] = 'Pressiona ~INPUT_PICKUP~ para trabalhares.',
+  ['dj_tip'] = 'DJ Tips',
+  ['dj_blip'] = 'Dj Party',
+  ['dj_cash_tips'] = 'Pressiona ~INPUT_PICKUP~ para trocar as tips.',
+}
